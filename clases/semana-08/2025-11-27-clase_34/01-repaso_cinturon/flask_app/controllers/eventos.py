acf0b69e1f7c696b73955c70864aa380738a9593
Diego <diego.canalesv@gmail.com>
2025-11-27 21:57:05 -0300

    avance

--- clases/semana-08/2025-11-27-clase_34/01-repaso_cinturon/flask_app/controllers/eventos.py
+++ clases/semana-08/2025-11-27-clase_34/01-repaso_cinturon/flask_app/controllers/eventos.py
@@ -1,6 +1,8 @@
-from flask_app import app
-from flask import render_template, redirect, flash, request
+from flask_app import app, bcrypt
+from flask import render_template, redirect, flash, request, session
 from flask_app.models.evento import Evento
+from flask_app.models.usuario import Usuario
+
 
 @app.route("/")
 def index():
@@ -10,6 +12,10 @@ def index():
 
 @app.route("/eventos")
 def mostrar_eventos():
+    if "usuario_id" not in session: # Si no hay id de usuario en la session
+        flash("Debes iniciar sesión para acceder", "error")
+        return redirect("/")
+
     # TODO: hay que hacer algo
     todos_los_eventos = Evento.get_all_eventos_y_usuarios()
     return render_template("eventos.html", eventos=todos_los_eventos)
@@ -17,6 +23,9 @@ def mostrar_eventos():
 
 @app.route("/nuevo")
 def nuevo_evento():
+    if "usuario_id" not in session: # Si no hay id de usuario en la session
+        flash("Debes iniciar sesión para acceder", "error")
+        return redirect("/")
     return render_template("nuevo_evento.html")
 
 
@@ -45,6 +54,9 @@ def crear_evento():
 
 @app.route("/ver/<int:id>")
 def ver_evento(id):
+    if "usuario_id" not in session: # Si no hay id de usuario en la session
+        flash("Debes iniciar sesión para acceder", "error")
+        return redirect("/")
     data = {"id": id}
     evento = Evento.get_evento_y_usuario(data)
     if evento is None:
@@ -55,6 +67,9 @@ def ver_evento(id):
 
 @app.route("/editar/<int:id>")
 def editar_evento(id):
+    if "usuario_id" not in session: # Si no hay id de usuario en la session
+        flash("Debes iniciar sesión para acceder", "error")
+        return redirect("/")
     evento = Evento.get_one(id)
     if not evento:
         redirect("/eventos")
@@ -82,3 +97,61 @@ def eliminar_evento(id):
     if evento:
         Evento.delete(id)
     return redirect("/eventos")
+
+
+# Login
+
+@app.route("/usuarios/registrar", methods=["POST"])
+def registrar():
+    # Validar datos
+    if not Usuario.validar_registro(request.form.to_dict()):
+        return redirect("/")
+    
+    # Hashear contraseña
+    pass_hasheado = bcrypt.generate_password_hash(request.form['password'])
+    
+    # Preparar datos
+    datos = {
+        "nombre": request.form['nombre'],
+        "apellido": request.form['apellido'],
+        "email": request.form['email'],
+        "password": pass_hasheado
+    }
+    
+    # Guardar usuario
+    nuevo_id = Usuario.save(datos)
+    
+    flash("¡Registro exitoso!", "exito")
+    
+    return redirect("/")
+
+
+@app.route("/usuarios/login", methods=["POST"])
+def login():
+    email_form = request.form["email"]
+    usuario = Usuario.buscar_por_email(email_form)
+
+    if not usuario: # Si no existe el usuario / Si el usuario no existe
+        flash("E-mail no está registrado", "login")
+    
+    # Si el usuario existe continua acá
+
+    # Verificar contraseña
+    password_hash_db = usuario.password # Hash
+    password_form = request.form["password"] # Texto plano
+    if not bcrypt.check_password_hash(password_hash_db, password_form): # Si el hash y la contraseña del form no hacen match
+        flash("Contraseña incorrecta", "login")
+        return redirect("/")
+    
+    # Si el hash y la contraseña del form hacen match continua acá
+    # Crear sesión
+    session["usuario_id"] = usuario.id
+    flash("Bienvenido!", "exito")
+    return redirect("/eventos")
+
+
+@app.route("/usuarios/logout", methods= ["POST"])
+def logout():
+    session.clear()
+    flash("Session cerrada!", "exito")
+    return redirect("/")
--- clases/semana-08/2025-11-27-clase_34/01-repaso_cinturon/flask_app/models/usuario.py
+++ clases/semana-08/2025-11-27-clase_34/01-repaso_cinturon/flask_app/models/usuario.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
 
 class Usuario:
     def __init__(self, data):
@@ -30,5 +31,48 @@ class Usuario:
         pass
 
     @classmethod
-    def save(cls, data):
-        pass
+    def save(cls, datos):
+        if "password" in datos and datos["password"]:
+            query = """
+                INSERT INTO usuarios
+                (nombre, apellido, email, password)
+                VALUES (%(nombre)s, %(apellido)s, %(email)s, %(password)s)
+            """
+        else:
+            query = """
+                INSERT INTO usuarios
+                (nombre, apellido, email)
+                VALUES (%(nombre)s, %(apellido)s, %(email)s)
+            """
+        nuevo_id = connectToMySQL('esquema_eventos').query_db(query, datos)
+        return nuevo_id
+
+
+    @staticmethod
+    def validar_registro(data):
+        es_valido = True
+
+        # TODO: implementar validaciones
+
+        if len(data.get("password", "").strip()) < 8:
+            flash("La contraseña debe tener al menos 8 caracteres", "password")
+            es_valido = False
+        
+        if data["password"] != data["confirm_password"]:
+            flash("Las contraseñas no coinciden", "password")
+            es_valido = False
+        
+        return es_valido
+
+
+    @classmethod
+    def buscar_por_email(cls, email):
+        datos = {
+            "email": email
+        }
+        query = "SELECT * FROM usuarios WHERE email = %(email)s"
+        resultado = connectToMySQL("esquema_eventos").query_db(query, datos)
+
+        if len(resultado) > 0:
+            return cls(resultado[0])
+        return None
